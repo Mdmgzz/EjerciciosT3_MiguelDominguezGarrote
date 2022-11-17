e470f91290eba288468eba08fddd93024e2fec48
mdominguezg <mdominguezg@106-06.info2>
2022-11-17 13:33:29 +0100

    commit

--- EjerciciosT3_MiguelDominguez/src/ejercicios/Ejercicio08.java
+++ EjerciciosT3_MiguelDominguez/src/ejercicios/Ejercicio08.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Ejercicio08 {
 
 	public static void main(String[] args) {
+		/*
+		 * Pruebas:
+		 * He probado con el:
+		 * 2= te pide el siguiente numero
+		 * 1= te da error
+		 * 6= continuas con el programa
+		 * 3= da error 
+		 * 0= se sale del programa
+		 */
 		// bloque de variables
 		int numinicial;				// guarda el numero que introduzca el usuario
 		int num = 1;
@@ -21,20 +30,20 @@ public class Ejercicio08 {
 		}else {
 			//iniciamos el bucle
 			
-			while (num!=0) {
-				contador++;
-				System.out.println("Dime un numero: ");
-				num=sc.nextInt();
+			while (num!=0) {								// el bucle funciona mientras este es distinto de 0 y positivo
+				contador++;									// aumenta el contador para saber los numeros que llevamos introducidos
+				System.out.println("Dime un numero: ");		// te pide el numero
+				num=sc.nextInt();	
 			
-				if (num>numinicial) {
-					numinicial=num;
+				if (num>numinicial){						// comprueba el el numero introducido es mayor que el inicial y si lo es,					
+					numinicial=num;							// pone como numero inicial al mayor
 				}else {
-					System.out.println("ERROR.");
-					contadorfallos++;
+					System.out.println("ERROR.");			// si no es mayor, te devuelve error y te vuelve a preguntar.
+					contadorfallos++;						// suma 1 al contador de fallos 
 				}
 			}
-			System.out.println("has introducido "+contador+" numeros.");
-			System.out.println("has cometido "+contadorfallos+" fallos.");
+			System.out.println("has introducido "+contador+" numeros.");		// muestra el numero de veces que hemos introducido numeros
+			System.out.println("has cometido "+contadorfallos+" fallos.");		// y los fallos
 		}
 		// cerramos scanner
 		sc.close();
